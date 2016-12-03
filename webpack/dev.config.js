6fa6435c0a6008e71b6c4b890fa33a96170a9b01
Engine Seunghyun Park <engine@zoyi.co>
2016-12-03 22:20:28 +0900

    Add build configuration

--- webpack/dev.config.js
+++ webpack/dev.config.js
@@ -1,7 +1,5 @@
 var webpack = require('webpack');
 var path = require('path');
-var ExtractTextPlugin = require("extract-text-webpack-plugin");
-var autoprefixer = require('autoprefixer')
 
 var BUILD_DIR = path.resolve(__dirname, '../build');
 var APP_DIR = path.resolve(__dirname, '../src');
@@ -9,22 +7,14 @@ var APP_DIR = path.resolve(__dirname, '../src');
 var config = {
   devtool: "source-map",
   entry: [
-    'webpack-dev-server/client?http://localhost:6001',
-    'webpack/hot/only-dev-server',
     APP_DIR + '/main.js',
     
   ],
   output: {
     path: BUILD_DIR,
-    filename: 'bundle.js'
+    filename: 'gimci.js'
   },
-  plugins: [
-    new ExtractTextPlugin('style.css', { allChunks: true }),
-    new webpack.HotModuleReplacementPlugin(),
-    new webpack.ProvidePlugin({
-      'fetch': 'imports?this=>global!exports?global.fetch!whatwg-fetch'
-    })
-  ],
+  plugins: [],
   resolve: {
     extensions: ['', '.js', '.jsx']
   },
@@ -35,7 +25,7 @@ var config = {
         exclude: /node_modules/,
         loader: 'babel-loader',
         query: {
-          presets:[ 'es2015', 'react', 'stage-2' ]
+          presets: ['es2015']
         }
       },
       {
@@ -43,36 +33,10 @@ var config = {
         exclude: /node_modules/,
         loader: 'babel-loader',
         query: {
-          presets:[ 'es2015', 'react', 'stage-2' ]
+          presets: ['es2015']
         }
-      },
-      {
-        test: /\.scss$/,
-        loader: ExtractTextPlugin.extract('style', 'css?modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]!sass!postcss-loader')
-      },
-      { test: /\.css$/, 
-        loader: ExtractTextPlugin.extract("style-loader", "css-loader") 
-      },
-      {
-        test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-        loader: "url-loader?limit=10000&mimetype=application/font-woff"
-      },
-      {
-        test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-        loader: "file-loader"
-      },
-      {
-        test: /\.png$/,
-        loader: 'url-loader?limit=10000&mimetype=image/png'
       }
     ]
-  },
-  devServer: {
-    historyApiFallback: {
-      index: 'index.html'
-    },
-    headers: { "Access-Control-Allow-Origin": "*" },
-    postcss: [ autoprefixer({ browsers: ['last 2 versions'] }) ]
   }
 };
 
