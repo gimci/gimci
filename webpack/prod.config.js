7e659852595a8c385bde9454e5a0cf5d92d186e8
Engine Seunghyun Park <engine@zoyi.co>
2016-12-03 20:38:43 +0900

    Set the configuration

---
+++ webpack/prod.config.js
@@ -0,0 +1,85 @@
+var webpack = require('webpack')
+var autoprefixer = require('autoprefixer')
+var ExtractTextPlugin = require("extract-text-webpack-plugin");
+
+module.exports = {
+  entry: {
+    app: './src/main.js'
+  },
+  output: {
+    path: './build',
+    filename: "bundle.js"
+  },
+  plugins: [
+    new ExtractTextPlugin('style.css', {allChunks: true}),
+    new webpack.NoErrorsPlugin(),
+    new webpack.optimize.UglifyJsPlugin({minimize: true}),
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': '"production"'
+    })
+  ],
+  resolve: {
+    modulesDirectories: ['node_modules']
+  },
+  node: {
+    net: 'empty',
+    dns: 'empty',
+    fs: 'empty'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: {
+          presets:[ 'es2015', 'react', 'stage-2' ]
+        }
+      },
+
+      {
+        test: /\.js?$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: {
+          presets:[ 'es2015', 'react', 'stage-2' ]
+        }
+      },
+
+      {
+        test: /\.scss$/,
+        loader: ExtractTextPlugin.extract('style', 'css?modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]!sass!postcss-loader')
+      },
+
+      {
+        test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        loader: "file-loader"
+      },
+
+      {
+        test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        loader: "url-loader?limit=10000&mimetype=application/font-woff"
+      },
+
+      {
+        test: /\.(png|jpg)$/,
+        loader: 'url-loader?limit=8192'
+      },
+
+      {
+        test: /\.html$/,
+        loader: "file?name=[path][name].[ext]&context=./src"
+      },
+
+      {
+        test: /\.json$/,
+        loader: 'json-loader'
+      },
+
+      { test: /\.css$/,
+        loader: ExtractTextPlugin.extract("style-loader", "css-loader")
+      }
+    ]
+  },
+  postcss: [ autoprefixer({ browsers: ['last 2 versions'] }) ]
+}
