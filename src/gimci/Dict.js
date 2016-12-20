623bb9854966e652cc84f96f097f95563d3807b0
Engine Seunghyun Park <engine@zoyi.co>
2016-12-20 17:21:24 +0900

    Add file write in Dict

--- src/gimci/Dict.js
+++ src/gimci/Dict.js
@@ -1,24 +1,39 @@
 import warning from './utils/warning'
 import romanize from './romanize'
 import fs from 'fs'
+import path from 'path'
 
 
 class Dict {
 
   constructor() {
-
+    this._path = ''
   }
 
-  read(srcPath) {
+  read(srcPath = '../../data/data.txt') {
+    this._path = this.resolvePath(srcPath)
     fs.readFile(srcPath, 'utf8', function (err, data) {
-        if (err) throw err;
+        if (err) {
+          throw err;
+        }
+        console.log('file read', data)
         return data;
       }
     );
   }
 
   write() {
+    const destPath = `${this._path}/parsed.txt`
+    fs.writeFile(destPath, "Hey there!", function(err) {
+      if(err) {
+        return console.log(err);
+      }
+      console.log("The file was saved!");
+    });
+  }
 
+  resolvePath(srcPath) {
+    return path.join(srcPath, '..')
   }
 }
 
