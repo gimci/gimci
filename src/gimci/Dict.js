21831b3b371d6d6b8768a5541970a40ee163d11f
uenieng <wonyeong91@naver.com>
2016-12-25 17:16:22 +0900

    Modify makeDict()-singleDeletion

--- src/gimci/Dict.js
+++ src/gimci/Dict.js
@@ -18,39 +18,45 @@ class Dict {
     let dict = {}
     const _data = File.read()
     const data = _data.match(/[^\r\n]+/g);
-    console.log(1, data)
 
     let tokens = {}
-    let token = ''
+    let base = ''
 
     data.map(elem => {
       tokens = GenerateToken.byDeletion(elem)
-      // base
-      token = tokens['base']
-      dict[token] = { refer: dict[token] }
+      base = tokens['base']
+      dict[base] = { refer: [ base ] }
+      // console.log(1, base, tokens['delete1'])
 
       // Delete single
       tokens['delete1'].map(delete1Elem => {
+        // console.log(3, delete1Elem)
         // If it does not exists,
         if (!dict[delete1Elem]) {
-          // todos: do something
+          // console.log(33, dict[delete1Elem])
+          dict[delete1Elem] = { refer: [base] }
         } else {
-          // ...
+          // console.log(44, dict[delete1Elem])
+          dict[delete1Elem]['refer'].push(base)
+          // if(!dict[delete1Elem]['refer'])
+          // {
+          //   dict[delete1Elem] = { refer: [base] }
+          // } else {
+          //   dict[delete1Elem]['refer'].push(base)
+          // }
         }
       })
 
       // Delete twice
 
-
-
-
     })
-    console.log(1, dict)
+    console.log(3, dict)
   }
 
   write(data) {
     return File.write(data)
   }
+
 
   temp() {
     return GenerateToken
