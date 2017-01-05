5d09f92f46488ea0506c820037e85822264c972c
Engine <enginehenryed@gmail.com>
2017-01-05 04:00:28 -0700

    Fix node modules to be discarded when unavailable

--- src/utils/FileUtils.js
+++ src/utils/FileUtils.js
@@ -1,34 +1,51 @@
 /**/
-import fs from 'fs'
-import path from 'path'
-
 let _srcPath = '../../assets/elementaryKorean.dict.json'
+
+
+/* Sanity check */
+try {
+  let fs = require('fs');
+  console.log("node js is in host environment");
+} catch (err){
+  console.log("Cannot load node modules")
+  let fs = false
+}
+
 
 /**
  *
  */
 const read = (srcPath = _srcPath) => {
-  return (fs.readFileSync(srcPath, 'utf8'))
+  return fs ? (fs.readFileSync(srcPath, 'utf8')) : null
 }
 
 /**
  *
  */
 const write = (destPath, data) => {
-  fs.writeFile(destPath, JSON.stringify(data), function(err) {
-    if(err) {
-      return console.log(err);
-    }
-    console.log("The file was written.");
-  });
+  if (fs) {
+    fs.writeFile(destPath, JSON.stringify(data), function(err) {
+      if(err) {
+        return console.log(err);
+      }
+      console.log("The file was written.");
+    });
+  } else {
+   // do nothing
+  }
 }
 
 /**
  *
  */
 const resolvePath = (srcPath) => {
-  console.log('path resolved', srcPath)
-  path.join(srcPath, '..')
+  if (fs) {
+    console.log('path resolved', srcPath)
+    let path = require('path')
+    path.join(srcPath, '..')
+  } else {
+    // do nothing
+  }
 }
 
 export default {
