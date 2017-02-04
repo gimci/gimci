aed612d9ea847e2cc45be7afc17aa442a71e7417
enginehenryed <enginehenryed@gmail.com>
2017-02-04 22:45:24 +0900

    DictUtils are undergoing a big change in structure and algorithm

--- src/utils/DictUtils.js
+++ src/utils/DictUtils.js
@@ -2,43 +2,129 @@
 import String from './StringUtils'
 import File from './FileUtils'
 import conf from '../conf'
+import readline from 'readline'
+import { log } from './LogUtils'
+import fs from 'fs'
 
 
 /**
- * This will create a JSON format file with all the populated data.
+ * Pipeline for building a dictionary file.
+ */
+
+/**
+ * Stage 0
+ * Romanized data into a linear data chunk.
+ *
+ * e.g. The content of the output file should be as following.
+ *   foo -refer foo
+ *   fo -refer foo
+ *   poo -refer poo
+ *   po -refer po
+ *   ...
  */
 const build = (dictSrcPath) => {
-  let dict = new Dict()
-  const _data = File.read(conf.dictSrcPath)
-  const data = _data.match(/[^\r\n]+/g) // separate by 'new line' characters
-  let tokens = {}
 
-  data.map(word => {
-    word = String.removeWhiteSpaces(word)
-    tokens = String.createTokensByDeletion(word)
+  stage0(dictSrcPath)
 
-    dict.insert(word, word)
-
-    tokens['delete1'].map(del1Word => {
-      dict.insert(del1Word, word)
-    })
-
-    tokens['delete2'].map(del2Word => {
-      dict.insert(del2Word, word)
-    })
-  })
-
-  File.write(conf.dictDestPath, dict.getDict())
+  // File.write(conf.dictDestPath, dict.getDict())
   console.log('Finished building new dictionary', conf.dictDestPath)
 }
 
 
 /**
+ *
+ */
+const stage0 = (dictSrcPath) => {
+  let tokens = {}
+  let rl = readline.createInterface({
+    input: fs.createReadStream(conf.dictSrcPath)
+  })
+  let ws = fs.createWriteStream(conf.dict0DestPath, {'flags': 'a'})
+  let i, j
+
+  rl.on('line', function(line) {
+    line = String.removeWhiteSpaces(line) // for trailing space at the beginning of file
+    tokens = String.createTokensByDeletion(line)
+
+    ws.write(`${line} -refer ${tokens.base}\n`) // write base
+    for (i = 0; i < tokens.del1.length; i++) {
+      ws.write(`${tokens.del1[i]} -refer ${tokens.base}\n`) // write del1 tokens
+    }
+    for (j = 0; j < tokens.del2.length; j++) {
+      ws.write(`${tokens.del2[j]} -refer ${tokens.base}\n`) // write del2 tokens
+    }
+  })
+    .on('close', function(err) {
+      log('end stage0', err)
+
+      // stage1
+      stage1()
+    })
+}
+
+/**
+ *
+ */
+const stage1 = (somePath) => {
+
+  let entries = [] // Checked entry will go into the list
+
+  let rl = readline.createInterface({
+    input: fs.createReadStream(conf.dictSrcPath)
+  })
+  let ws = fs.createWriteStream(conf.dict1DestPath, {'flags': 'a'})
+
+  // during some condition,
+  let cond = true
+  while (cond) {
+    rl.on('line', function(line) {
+      // if 'line' is not already taken entry, start processing
+      // add it to the 'entries'
+
+      // process it
+    })
+      .on('close', function() {
+        // process again with the other entry
+        sumEntryInstances(rl, ws)
+      })
+  }
+}
+
+/**
+ *
+ */
+const sumEntryInstances = (rl, ws) => {
+
+  // Create a readstream again,
+  rl = readline.createInterface({
+    input: fs.createReadStream(conf.dictSrcPath)
+  })
+
+  rl.on('line', function(line) {
+    // if 'line' is not already taken entry, start processing
+    // add it to the 'entries'
+
+    // process it
+  })
+    .on('close', function() {
+      // process again with the other entry
+    })
+}
+
+
+
+
+
+
+
+
+/**
+ * DEPRECATED
+ *
  * Inner dictionary class that holds the data.
  * This class will not be seen from the outside.
  */
 class Dict {
-
   constructor() {
     this._dict = {}
   }
@@ -63,7 +149,6 @@ class Dict {
   getDict() {
     return this._dict
   }
-
 }
 
 
